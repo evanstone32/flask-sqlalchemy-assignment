95fa4f8b6995dc075d4099ab7fc2064732457afe
evanstone32 <evanstone32@gmail.com>
2023-04-07 19:53:52 -0400

    Final Commit

--- src/models.py
+++ src/models.py
@@ -4,7 +4,13 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Movies(db.Model):
+    db = SQLAlchemy()
+
     movie_id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(255), nullable=False)
     director = db.Column(db.String(255), nullable=False)
     rating = db.Column(db.Integer, nullable=False) 
+
+        
+
+    
--- src/repositories/movie_repository.py
+++ src/repositories/movie_repository.py
@@ -1,25 +1,28 @@
 
-from src.models import Movies,db
+from src.models import Movies, db
 class MovieRepository:
 
     
 
     def get_all_movies(self):
         # TODO get all movies from the DB
-        movies = Movies(db.Model)
-        return movies.query.all()
+        
+        return Movies.query.all()
 
     def get_movie_by_id(self, movie_id):
         # TODO get a single movie from the DB using the ID
-        return None
+        return Movies.query.get(movie_id)
 
     def create_movie(self, title, director, rating):
         # TODO create a new movie in the DB
-        return None
+        new_movie = Movies(title=title, director=director, rating=rating)
+        db.session.add(new_movie)
+        db.session.commit()
+        return new_movie
 
     def search_movies(self, title):
         # TODO get all movies matching case insensitive substring (SQL LIKE, use google for how to do with SQLAlchemy)
-        return None
+        return Movies.query.filter(Movies.title.ilike(f"%{title}%")).all()
 
 
 # Singleton to be used in other modules
